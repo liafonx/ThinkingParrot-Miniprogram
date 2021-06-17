16b3f1ea1529035a7cba77445e22868d5bf543e6
Xu Yinong <liafonx@outlook.com>
2021-06-18 01:07:46 +0800

    update

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,11 @@
 //app.js
 var jsonList = require('data/json.js');
+var oralList = require('data/oral.js')
 
 App({
   globalData: {
     questionList: jsonList.questionList,
+    oralList: oralList.oralList,
     questionDone: 0,
     wrongDone: 0,
   }
---
+++ miniprogram/data/oral.js
@@ -0,0 +1,40 @@
+var json = {
+  "unit2": [
+    {
+      "question": "我会对结果负责",
+      "key": "The consequences are on me.",
+      "scores": 10, 
+      "checked": false
+    },
+    {
+      "question": "你输了",
+      "key": "You are on.",
+      "scores": 10, 
+      "checked": false
+    },
+    {
+      "question": "我和她合不来",
+      "key": "I lost her at hello.",
+      "scores": 10, 
+      "checked": false
+    },
+    {
+      "question": "How to say \"人人为己\" ？",
+      "key": "Everyone is out for himself.",
+      "scores": 10, 
+      "checked": false
+    },
+    {
+      "question": "赚钱",
+      "key": "In the black",
+      "scores": 10, 
+      "checked": false
+    },
+  ],
+
+  
+}
+
+module.exports = {
+  oralList: json
+}
